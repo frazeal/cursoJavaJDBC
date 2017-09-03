0c6d0ddb51d0b51b16e02288e82bab5a42b3e3be
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-09-03 20:35:53 -0300

    Adiciona aula 4

--- src/br/com/caelum/testes/TestaInsercaoParametros.java
+++ src/br/com/caelum/testes/TestaInsercaoParametros.java
@@ -11,26 +11,46 @@ import br.com.caelum.jdbc.DataBase;
 public class TestaInsercaoParametros {
 	public static void main(String[] args) throws SQLException {
 		try (Connection conexao = DataBase.getConnection()) {
-			String nome = "Microondas";
-			String descricao = "Mircroondas branco de 20 litros.";
-
+			conexao.setAutoCommit(false);
 			String sqlString = "INSERT INTO PRODUTOS (NOME, DESCRICAO) VALUES (?, ?)";
 
-			PreparedStatement sqlQuery = conexao.prepareStatement(sqlString, Statement.RETURN_GENERATED_KEYS);
-			sqlQuery.setString(1, nome);
-			sqlQuery.setString(2, descricao);
+			try (PreparedStatement sqlQuery = conexao.prepareStatement(sqlString,
+					Statement.RETURN_GENERATED_KEYS);) {
 
-			boolean resultado = sqlQuery.execute();
-			System.out.println(resultado);
+				adicionarRegistro("Notebook'i5", "Notebook i5", sqlQuery);
+				adicionarRegistro("TV LCD", "32 polegadas", sqlQuery);
+				adicionarRegistro("Blueray", "full hd", sqlQuery);
+				conexao.commit();
+				
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+				conexao.rollback();
+				System.out.println("Rollback efetuado");
+				
+			}
+		}
+	}
 
-			ResultSet registros = sqlQuery.getGeneratedKeys();
+	private static void adicionarRegistro(String nome, String descricao, PreparedStatement sqlQuery)
+			throws SQLException {
+
+		if (nome.equals("Blueray")) {
+			throw new IllegalArgumentException("Simulando erro em transação sql.");
+		}
+
+		sqlQuery.setString(1, nome);
+		sqlQuery.setString(2, descricao);
+
+		boolean resultado = sqlQuery.execute();
+		System.out.println(resultado);
+
+		try (ResultSet registros = sqlQuery.getGeneratedKeys()) {
 			while (registros.next()) {
 				int id = registros.getInt("id");
 				System.out.println(id + " gerado");
 			}
-
-			registros.close();
-			sqlQuery.close();
 		}
+		
 	}
 }
